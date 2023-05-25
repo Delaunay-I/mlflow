09601a26116f97b789a06fdcdd6e9331b99d63a2
Amirhossein <amirmsh@student.ubc.ca>
2023-05-24 17:14:12 -0700

    answered Q4

--- notebooks/hw/hw2/hpo.py
+++ notebooks/hw/hw2/hpo.py
@@ -34,6 +34,7 @@ def run_optimization(data_path: str, num_trials: int):
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
     def objective(trial):
+
         params = {
             'n_estimators': trial.suggest_int('n_estimators', 10, 50, 1),
             'max_depth': trial.suggest_int('max_depth', 1, 20, 1),
@@ -43,10 +44,14 @@ def run_optimization(data_path: str, num_trials: int):
             'n_jobs': -1
         }
 
-        rf = RandomForestRegressor(**params)
-        rf.fit(X_train, y_train)
-        y_pred = rf.predict(X_val)
-        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        with mlflow.start_run():
+            mlflow.set_tag("model", "xgboost")
+            mlflow.log_params(params)
+            rf = RandomForestRegressor(**params)
+            rf.fit(X_train, y_train)
+            y_pred = rf.predict(X_val)
+            rmse = mean_squared_error(y_val, y_pred, squared=False)
+            mlflow.log_metric('rmse', rmse)
 
         return rmse
 
